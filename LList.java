0210c4d995f8981a41283c641ba3639e646ad191
Rowena Singh <rowenacsingh@gmail.com>
2017-05-02 11:44:20 -0400

    added pushFront, printer, printLength, and pop methods

--- LList.java
+++ LList.java
@@ -35,33 +35,68 @@ class LinkedList {
 		}
 		else {
 			int i = 1;
-			Node ob1 = head;
-			while (ob1.next != null) {
-				ob1 = ob1.next;
+			Node ob = head;
+			while (ob.next != null) {
+				ob = ob.next;
 				i++;
 			}
 			return i;
 		}
 	}
-	// pushFront(int val) {
 
-	// }
+	void pushFront(int val) {
+		if (head == null) {
+			head = new Node(val);
+		}
+		else {
+			Node ob = new Node(val);
+			ob.next = head;
+			head = ob;
+		}
+	}
+
+	void printer() {
+		Node ob = head;
+		System.out.print(ob.data + "\t");
+		while (ob.next != null) {
+			ob = ob.next;
+			System.out.print(ob.data + "\t");
+		}
+		System.out.println();
+	}
+
+	void printLength() {
+		System.out.println("Length of list: " + this.length());
+		System.out.println();
+	}
+
+	int pop() {
+		int val = head.data;
+		head = head.next;
+		return val;
+	}
 }
 
 class LList {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		list.push(5);
-		System.out.println(list.head.data);
 		list.push(9);
-		System.out.println(list.head.next.data);
 		list.push(8);
-		System.out.println(list.head.next.next.data);
-
-		list.push(408795);
-		System.out.println(list.head.next.next.next.data);
-
 		list.push(10);
-		System.out.println(list.length());
+		list.printer();
+		list.printLength();
+
+		list.pushFront(1);
+		list.printer();
+		list.printLength();
+
+		System.out.println("Popped value: " + list.pop());
+		System.out.println();
+		list.printer();
+
+		System.out.println();
+		System.out.println("Current head: " + list.head.data);
+		list.printLength();
 	}
 }
