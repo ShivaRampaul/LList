fa10e6aa0e292b2cc71655b2db7a78fec6c6c0f8
Rowena Singh <rowenacsingh@gmail.com>
2017-05-03 12:02:36 -0400

    improved popBack and remove methods

--- LList.java
+++ LList.java
@@ -85,45 +85,32 @@ class LinkedList {
 
 	// removes val from end of list
 	int popBack() {
-		int i = 1;
 		Node ob = head;
-		// last object
-		while (ob.next != null) {
-			ob = ob.next;
-			i++;
-		}
-		int j = 1;
 		// second to last object
-		Node ob1 = head;
-		for (; j < i - 1; j++) {
-			ob1 = ob1.next;
+		while (ob.next.next != null) {
+			ob = ob.next;
 		}
-		int val = ob1.next.data;
-		ob1.next = null;
+		int val = ob.next.data;
+		ob.next = null;
 		return val;
 	}
 
 	// removes x from the list
 	boolean remove(int x) {
 		Node ob = head;
-		int i = 0;
+		boolean flag = false; //to keep track of whether or not the operation was done successfully
 		if (ob.data == x) {
 			head = ob.next;
 		}
 		while (ob.next != null) {
 			if (ob.next.data == x) {
 				ob.next = ob.next.next;
-				i++;
+				flag = true;
 				continue;
 			}
 			ob = ob.next;
 		}
-		if (i > 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
+		return flag;
 	}
 
 	// find x in list and returns true
@@ -167,23 +154,25 @@ class LList {
 		System.out.println("popping...");
 		System.out.println("Popped value: " + list.pop());
 		list.printer();
+		list.printLength();
 
 		System.out.println();
 
 		// removes object at end of list
-		System.out.println("popping...");
+		System.out.println("popping last object...");
 		System.out.println("Popped value: " + list.popBack());
 		list.printer();
 		list.printLength();
 
 		// removes x from the list and returns true 
-		System.out.println("removing...");
+		System.out.println("removing 9 from the list...");
 		System.out.println(list.remove(9));
 		list.printer();
+		list.printLength();
 
 		// finds a value and returns true
 		System.out.println();
-		System.out.println("finding...");
+		System.out.println("looking for 8...");
 		System.out.println(list.find(8));
 		list.printer();
 	}
